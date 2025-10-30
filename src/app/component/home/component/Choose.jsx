c61ea9098596e2a3a3ecc514e5d69667bf423d70
LinkPlayer047 <linkplayer047@gmail.com>
2025-10-30 16:13:50 +0500

    ok

--- src/app/component/home/component/Choose.jsx
+++ src/app/component/home/component/Choose.jsx
@@ -5,6 +5,30 @@ import { FaStar } from "react-icons/fa";
 
 const Choose = () => {
   return (
+    <>
+    <div className="relative">
+        <div className="relative h-16 sm:h-28 md:h-36 lg:h-48">
+          <svg
+            viewBox="0 0 1920 863"
+            className="absolute bottom-0 left-0 w-full h-full"
+            preserveAspectRatio="none"
+          >
+            <path
+              d="M-3,551 C186.257589,757.321118 319.044414,856.322454 
+             395.360475,848.004007 C509.834566,835.526337 
+             561.525143,796.329212 637.731734,765.961549 
+             C713.938325,735.593886 816.980646,681.910577 
+             1035.72208,733.065469 C1254.46351,784.220361 
+             1511.54925,678.92359 1539.40808,662.398665 
+             C1567.2669,645.87374 1660.9143,591.478574 
+             1773.19378,597.641868 C1848.04677,601.75073 
+             1901.75645,588.357675 1934.32284,557.462704 
+             L1934.32284,863.183395 L-3,863.183395"
+              fill="#FFFFFF"
+            />
+          </svg>
+        </div>
+      </div>
     <div className="w-full bg-[#00205b] flex items-center justify-center py-20">
       <div className="mycontainer flex flex-col items-center justify-center gap-3">
         <p className="text-white uppercase">
@@ -59,6 +83,7 @@ const Choose = () => {
         </div>
       </div>
     </div>
+    </>
   );
 };
 
